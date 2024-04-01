2f92dc7b226059b331a3a0aba833b96f9f43d7a8
김우주 <https://github.com/ghvhdh333>
2024-04-01 15:32:42 +0900

    fix/info

--- pages/info/[id].js
+++ pages/info/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import Seo from "@/components/Seo";
-import { useState, useEffect } from "react" 
+import { useState, useEffect } from "react";
 
 export default function Info() {
   const router = useRouter();
@@ -9,63 +9,64 @@ export default function Info() {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    if(id){
+    if (id) {
       (async () => {
-        const data = await (
-          await fetch(`/api/movie/info/${id}`)).json();
+        const data = await (await fetch(`/api/movie/info/${id}`)).json();
         setMovie(data);
       })();
     }
   }, [id]);
- 
-console.log(movie)
- return (
-     <div className='container'>
+
+  return (
+    <div className="container">
       <Seo />
-        {movie.length !== 0 && id ? 
+      {movie.length !== 0 && id ? (
         <>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-            <section className="movie_detail">
-                <div className="detail_items">
-                    <div className="detail_box">Title</div>
-                    <span className="detail_text">{movie.title}</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Movie ID</div>
-                    <span className="detail_text">{movie.id}</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Language</div>
-                    <span className="detail_text">{movie.original_language}</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Release Date</div>
-                    <span className="detail_text">{movie.release_date}</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Runtime</div>
-                    <span className="detail_text">{movie.runtime} min</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Vote Average</div>
-                    <span className="detail_text vote_average">{movie.vote_average}</span>
-                    <span> / 10</span>
-                </div>
-                <div className="detail_items">
-                    <div className="detail_box">Vote Count</div>
-                    <span className="detail_text">{movie.vote_count} 🙋‍♂️</span>
-                </div>
-                <div>
-                    <div className="detail_box">Overview</div>
-                    <div className="detail_overview">{movie.overview}</div>
-                </div>
-            </section>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <section className="movie_detail">
+            <div className="detail_items">
+              <div className="detail_box">Title</div>
+              <span className="detail_text">{movie.title}</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Movie ID</div>
+              <span className="detail_text">{movie.id}</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Language</div>
+              <span className="detail_text">{movie.original_language}</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Release Date</div>
+              <span className="detail_text">{movie.release_date}</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Runtime</div>
+              <span className="detail_text">{movie.runtime} min</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Vote Average</div>
+              <span className="detail_text vote_average">
+                {movie.vote_average}
+              </span>
+              <span> / 10</span>
+            </div>
+            <div className="detail_items">
+              <div className="detail_box">Vote Count</div>
+              <span className="detail_text">{movie.vote_count} 🙋‍♂️</span>
+            </div>
+            <div>
+              <div className="detail_box">Overview</div>
+              <div className="detail_overview">{movie.overview}</div>
+            </div>
+          </section>
         </>
-      : <h2 className="loading">Loading...</h2>
-    }
+      ) : (
+        <h2 className="loading">Loading...</h2>
+      )}
       <style jsx>{`
         .container {
-          height : 89vh;
+          height: 89vh;
           display: flex;
           flex-wrap: wrap;
           justify-content: center;
@@ -75,60 +76,60 @@ console.log(movie)
         }
 
         img {
-            width: 500px;
-            border-radius: 12px;
-            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          width: 500px;
+          border-radius: 12px;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
 
         .detail_items {
-            display: flex;
-            justify-content: start;
-            align-items: center;
+          display: flex;
+          justify-content: start;
+          align-items: center;
         }
-        
+
         .movie_detail {
-            display:flex;
-            flex-direction: column;
-            gap: 20px;
+          display: flex;
+          flex-direction: column;
+          gap: 20px;
         }
 
         .detail_box {
-            border-radius: 12px;
-            padding:10px 20px 10px 20px;
-            background-color: gray;
-            color: white;
-            
-            font-size: 20px;
-            font-weight: 600;
-            text-align: center;
+          border-radius: 12px;
+          padding: 10px 20px 10px 20px;
+          background-color: gray;
+          color: white;
 
-            margin-right: 10px;
-            width: 150px;
+          font-size: 20px;
+          font-weight: 600;
+          text-align: center;
+
+          margin-right: 10px;
+          width: 150px;
         }
 
         .detail_text {
-            font-size: 20px;
-            font-weight: 600;
+          font-size: 20px;
+          font-weight: 600;
         }
 
         .vote_average {
-            padding-right: 5px;
+          padding-right: 5px;
         }
 
         .detail_overview {
-            font-size: 20px;
-            font-weight: 600;
-            padding: 10px;
-            flex-wrap: wrap;
-            width: 500px;   
+          font-size: 20px;
+          font-weight: 600;
+          padding: 10px;
+          flex-wrap: wrap;
+          width: 500px;
         }
 
         .loading {
           display: flex;
           justify-content: center;
-          align-items:center;
-          margin:0;
-          height:100vh;
+          align-items: center;
+          margin: 0;
+          height: 100vh;
           margin-top: -110px;
         }
       `}</style>
